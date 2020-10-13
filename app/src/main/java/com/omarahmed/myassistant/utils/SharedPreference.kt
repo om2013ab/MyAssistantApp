68ecbd8b8984382ed436682607557fac94f16062
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-13 10:30:16 +0800

    edit the boolean default value

--- app/src/main/java/com/omarahmed/myassistant/utils/SharedPreference.kt
+++ app/src/main/java/com/omarahmed/myassistant/utils/SharedPreference.kt
@@ -7,6 +7,6 @@ class SharedPreference(context: Context) {
 
     fun getStringValue(key:String, defValue:String) = sharedPreference.getString(key,defValue)
     fun putStringValue(key: String, value: String?) = sharedPreference.edit().putString(key,value).apply()
-    fun getBooleanValue(key: String) = sharedPreference.getBoolean(key,true)
+    fun getBooleanValue(key:String, defValue:Boolean) = sharedPreference.getBoolean(key,defValue)
     fun putBooleanValue(key: String, value: Boolean) = sharedPreference.edit().putBoolean(key,value).apply()
 }
