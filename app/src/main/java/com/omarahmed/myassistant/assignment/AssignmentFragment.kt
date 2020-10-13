591c45c01c3c08202d40fa247f6120e8ced10e86
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-13 10:19:56 +0800

    edit adding new assignment to be in a new fragment instead of using dialog

--- app/src/main/java/com/omarahmed/myassistant/assignment/AssignmentFragment.kt
+++ app/src/main/java/com/omarahmed/myassistant/assignment/AssignmentFragment.kt
@@ -1,40 +1,28 @@
 package com.omarahmed.myassistant.assignment
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
-import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.omarahmed.myassistant.R
-import com.omarahmed.myassistant.data.models.AssignmentInfo
-import com.omarahmed.myassistant.databinding.DialogAddAssignmentBinding
+import com.omarahmed.myassistant.alarmmanager.ScheduleAlarm.Companion.cancelAlarm
 import com.omarahmed.myassistant.databinding.FragmentAssignmentBinding
-import com.omarahmed.myassistant.home.HomeViewModel
-import com.omarahmed.myassistant.utils.Constants.Companion.DATE_PATTERN
-import com.omarahmed.myassistant.utils.DatePicker
-import com.omarahmed.myassistant.utils.TextWatcher
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 class AssignmentFragment : Fragment() {
     private val assignmentViewModel: AssignmentViewModel by viewModels()
-    private val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var assignmentAdapter: AssignmentAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         // Inflate the layout for this fragment
         val binding = FragmentAssignmentBinding.inflate(inflater)
-        val assignmentAdapter = AssignmentAdapter()
+        assignmentAdapter = AssignmentAdapter()
         binding.rvAssignments.adapter = assignmentAdapter
 
         assignmentViewModel.getAllAssignment.observe(viewLifecycleOwner, {
@@ -42,7 +30,7 @@ class AssignmentFragment : Fragment() {
             assignmentAdapter.assignmentList.submitList(it)
         })
         binding.floatingActionButton.setOnClickListener {
-            setupAddAssignmentDialog()
+            findNavController().navigate(R.id.action_assignmentFragment_to_addAssignmentFragment2)
         }
         binding.lifecycleOwner = this
         binding.assignmentViewModel = assignmentViewModel
@@ -52,54 +40,6 @@ class AssignmentFragment : Fragment() {
         return binding.root
     }
 
-    private fun setupAddAssignmentDialog() {
-        val builder = MaterialAlertDialogBuilder(requireContext())
-        val view = DialogAddAssignmentBinding.inflate(LayoutInflater.from(requireContext()))
-        builder.setView(view.root)
-        val dialog = builder.create()
-        dialog.apply {
-            show()
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
-        setupDropDownMenu(view)
-        view.deadline.setOnClickListener {
-            DatePicker.datePickerDialog(view.deadline, requireContext())
-        }
-
-        view.btnAdd.setOnClickListener {
-            insertAssignments(view, dialog)
-        }
-
-        view.btnCancel.setOnClickListener {
-            view.switchAssignment.isChecked = false
-            dialog.dismiss()
-        }
-        TextWatcher.textChangedAssignment(view)
-    }
-
-    private fun insertAssignments(view: DialogAddAssignmentBinding, dialog: AlertDialog) {
-        val code = view.codeAssignment.text.toString()
-        val deadline = SimpleDateFormat(DATE_PATTERN, Locale.US).parse(view.deadline.text.toString())
-        var description = view.description.text.toString()
-        if (description == "") description = "No description"
-        val notify = view.switchAssignment.isChecked
-        val newAssignment = AssignmentInfo(0, code, deadline, description, notify)
-        assignmentViewModel.insertAssignment(newAssignment)
-        dialog.dismiss()
-    }
-
-
-    private fun setupDropDownMenu(view: DialogAddAssignmentBinding) {
-        val codeList = ArrayList<String>()
-        homeViewModel.getAllCourses.observe(viewLifecycleOwner, Observer {
-            for (code in it) {
-                codeList.add(code.courseCode)
-            }
-            val adapter = ArrayAdapter(requireContext(), R.layout.drop_down_layout, codeList)
-            view.codeAssignment.setAdapter(adapter)
-        })
-
-    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.delete_all_menu, menu)
@@ -113,6 +53,9 @@ class AssignmentFragment : Fragment() {
                 .setMessage("Are you sure you want to delete all current assignments")
                 .setPositiveButton("YES") { _, _ ->
                     assignmentViewModel.deleteAllAssignments()
+                    assignmentAdapter.assignmentList.currentList.forEach {
+                        cancelAlarm(requireContext(),it.id)
+                    }
                 }
                 .setNegativeButton("NO") { _, _ ->
                 }
