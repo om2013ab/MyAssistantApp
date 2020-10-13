f63f5d553e414af88da8500f2c941a1b2ba8bac1
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-13 10:28:40 +0800

    adding notification id constants

--- app/src/main/java/com/omarahmed/myassistant/utils/Constants.kt
+++ app/src/main/java/com/omarahmed/myassistant/utils/Constants.kt
@@ -7,7 +7,7 @@ class Constants {
         const val API_KEY = "2cb6c150ce36b78b8938d758b4c41a54956f37f1"
         const val BASE_URL = "https://calendarific.com/"
         const val CHANNEL_ID = "my channel"
-        const val WORK_NAME = "NotificationWorker"
+        var NOTIFICATION_ID = 1
         const val CURRENT_YEAR = 2020
 
 
