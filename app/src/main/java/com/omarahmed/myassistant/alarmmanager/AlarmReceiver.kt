0c6449b5cd6ae20895ad884c44128eede86314fd
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-16 23:16:14 +0800

    enable notification after reboot the device

--- app/src/main/java/com/omarahmed/myassistant/alarmmanager/AlarmReceiver.kt
+++ app/src/main/java/com/omarahmed/myassistant/alarmmanager/AlarmReceiver.kt
@@ -1,35 +1,57 @@
 package com.omarahmed.myassistant.alarmmanager
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.omarahmed.myassistant.MainActivity
 import com.omarahmed.myassistant.R
 import com.omarahmed.myassistant.utils.Constants
 import com.omarahmed.myassistant.utils.Constants.Companion.NOTIFICATION_ID
 
-class AlarmReceiver: BroadcastReceiver(){
+class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
-        val code = intent?.getStringExtra("code")
-        val deadline = intent?.getStringExtra("deadline")
-        val notificationType = intent?.getStringExtra("type")
-        val message = if (notificationType == "assignment"){
-            "Your ($code) $notificationType will due in $deadline"
+        if (Intent.ACTION_BOOT_COMPLETED == intent?.action) {
+            val i = Intent(context, RestartAlarm::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(i)
+            } else {
+                context.startService(i)
+            }
         } else {
-            "Your ($code) $notificationType will be in $deadline"
+            val code = intent?.getStringExtra("name")
+            val deadline = intent?.getStringExtra("deadline")
+            val notificationType = intent?.getStringExtra("type")
+            val message = if (notificationType == "assignment") {
+                "Your ($code) $notificationType will due in $deadline"
+            } else {
+                "Your ($code) $notificationType will be in $deadline"
+            }
+
+
+            showNotification(context, message)
         }
 
-        showNotification(context,message)
     }
 
-    private fun showNotification(context: Context, body:String?){
+    private fun showNotification(context: Context, body: String?) {
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
         val notificationBuilder = NotificationCompat.Builder(context, Constants.CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_test)
+            .setSmallIcon(R.drawable.ic_assistant)
             .setContentTitle("Attention")
-            .setContentText(body)
+            .setColor(Color.BLUE)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(body))
+
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
         val notificationManager = NotificationManagerCompat.from(context)
-        notificationManager.notify(NOTIFICATION_ID,notificationBuilder.build())
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
         NOTIFICATION_ID++
     }
 }
