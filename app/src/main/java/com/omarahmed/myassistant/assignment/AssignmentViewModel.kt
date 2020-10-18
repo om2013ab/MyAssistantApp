d088a497b9901b9e425e80af4c6cc38337eb5494
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:19:59 +0800

    edit the location of AssignmentInfo.kt

--- app/src/main/java/com/omarahmed/myassistant/assignment/AssignmentViewModel.kt
+++ app/src/main/java/com/omarahmed/myassistant/assignment/AssignmentViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.omarahmed.myassistant.data.CoursesDatabase
-import com.omarahmed.myassistant.data.models.AssignmentInfo
 import com.omarahmed.myassistant.data.repository.AssignmentRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
