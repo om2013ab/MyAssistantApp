cd4afec1d6d5ce3b41c472088e66c0f9935c39fb
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:24:58 +0800

    delete the Countries table from room

--- app/src/main/java/com/omarahmed/myassistant/data/remote/CountriesResponse.kt
+++ app/src/main/java/com/omarahmed/myassistant/data/remote/CountriesResponse.kt
@@ -1,10 +1,7 @@
 package com.omarahmed.myassistant.data.remote
 
-import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+
 
 data class CountriesResponse(
     val response: Countries
@@ -12,17 +9,13 @@ data class CountriesResponse(
     data class Countries(
         val countries: List<CountriesInfo>
     ){
-        @Parcelize
-        @Entity(tableName = "countries_table")
         data class CountriesInfo(
             @SerializedName("country_name")
             val countryName: String?,
-            @SerializedName("iso-3166")
             val isoName: String?,
             @SerializedName("total_holidays")
             val totalHolidays: Int,
-            @PrimaryKey
             val uuid: String
-        ):Parcelable
+        )
     }
 }
