a30f38b304d947adc5161230b44f7deef270ec0e
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:22:39 +0800

    edit the location of TimetableInfo.kt

--- app/src/main/java/com/omarahmed/myassistant/data/dao/TimetableDao.kt
+++ app/src/main/java/com/omarahmed/myassistant/data/dao/TimetableDao.kt
@@ -2,7 +2,7 @@ package com.omarahmed.myassistant.data.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.omarahmed.myassistant.data.models.TimetableInfo
+import com.omarahmed.myassistant.timetable.TimetableInfo
 
 
 @Dao
