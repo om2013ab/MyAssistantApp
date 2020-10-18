6087dc68f5f2ed8bffd0a564dea17777b4dc554b
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:39:44 +0800

    edit the apiKey

--- app/src/main/java/com/omarahmed/myassistant/utils/Constants.kt
+++ app/src/main/java/com/omarahmed/myassistant/utils/Constants.kt
@@ -1,14 +1,17 @@
 package com.omarahmed.myassistant.utils
 
+import java.util.*
+
 class Constants {
+
     companion object{
         const val DATE_PATTERN = "MMM dd, yyyy"
         const val TIME_PATTERN = "hh:mm a"
-        const val API_KEY = "2cb6c150ce36b78b8938d758b4c41a54956f37f1"
+        const val API_KEY = "829b0a7c193c99bce060859028aa2d9cfad90a35"
         const val BASE_URL = "https://calendarific.com/"
         const val CHANNEL_ID = "my channel"
         var NOTIFICATION_ID = 1
-        const val CURRENT_YEAR = 2020
+        val CURRENT_YEAR = Calendar.getInstance().get(Calendar.YEAR)
 
 
     }
