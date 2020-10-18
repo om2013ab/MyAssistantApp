8240a818aedf837843720ee11f7224705174e7de
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:30:13 +0800

    remove the code of fetching data from room

--- app/src/main/java/com/omarahmed/myassistant/holiday/HolidayViewModel.kt
+++ app/src/main/java/com/omarahmed/myassistant/holiday/HolidayViewModel.kt
@@ -2,28 +2,25 @@ package com.omarahmed.myassistant.holiday
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
 import android.net.NetworkCapabilities.*
+import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.omarahmed.myassistant.MyApplication
-import com.omarahmed.myassistant.data.CoursesDatabase
-import com.omarahmed.myassistant.data.models.HolidayInfo
 import com.omarahmed.myassistant.data.remote.CountriesResponse
 import com.omarahmed.myassistant.data.remote.HolidayResponse
 import com.omarahmed.myassistant.data.repository.HolidayRepository
 import com.omarahmed.myassistant.utils.Resource
-import com.omarahmed.myassistant.utils.SharedPreference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import java.io.IOException
 
 class HolidayViewModel(application: android.app.Application) : AndroidViewModel(application) {
-    private val sharedPreference = SharedPreference(application.applicationContext)
-    private val db = CoursesDatabase.getDatabase(application.applicationContext)
-    private val repository = HolidayRepository(db)
+    private val repository = HolidayRepository()
 
     private val _holidaysApiResponse: MutableLiveData<Resource<HolidayResponse>> = MutableLiveData()
     val holidaysApiResponse: LiveData<Resource<HolidayResponse>>
@@ -33,9 +30,6 @@ class HolidayViewModel(application: android.app.Application) : AndroidViewModel(
     val countriesApiResponse: LiveData<Resource<CountriesResponse>>
         get() = _countriesApiResponse
 
-    val countriesFromRoom: LiveData<List<CountriesResponse.Countries.CountriesInfo>> =
-        repository.getCountriesFromRoom
-    val holidaysFromRoom: LiveData<List<HolidayInfo>> = repository.getHolidaysFromRoom
 
     fun getHolidaysFromApi(countryCode: String?, year: Int, month: Int) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -77,13 +71,8 @@ class HolidayViewModel(application: android.app.Application) : AndroidViewModel(
         }
     }
 
-
-
     private fun handleCountriesResponse(response: Response<CountriesResponse>): Resource<CountriesResponse>? {
         if (response.isSuccessful) {
-            response.body()?.response?.countries?.forEach {
-                insertCountries(it)
-            }
             val countriesResponse = response.body()
             countriesResponse?.let {
                 return Resource.Success(it)
@@ -104,29 +93,30 @@ class HolidayViewModel(application: android.app.Application) : AndroidViewModel(
 
     }
 
-
-    private fun insertCountries(countriesInfo: CountriesResponse.Countries.CountriesInfo) =
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.insertCountries(countriesInfo)
-        }
-
-    private fun insertHolidays(holidayInfo: HolidayInfo) =
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.insertHolidays(holidayInfo)
-        }
-
-    fun hasInternetConnection(): Boolean {
+    private fun hasInternetConnection(): Boolean {
         val connectivityManager = getApplication<MyApplication>().getSystemService(
             Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activityNetwork = connectivityManager.activeNetwork ?: return false
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(activityNetwork) ?: return false
-        return when {
-            capabilities.hasTransport(TRANSPORT_WIFI) -> true
-            capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
-            capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
-            else -> false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val activityNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(activityNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when(type){
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
         }
+        return false
     }
 
 }
