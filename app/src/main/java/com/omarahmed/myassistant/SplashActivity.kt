d06343994012916b78e99e4a25410a82343e011a
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-18 17:40:42 +0800

    create SplashActivity.kt class

---
+++ app/src/main/java/com/omarahmed/myassistant/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.omarahmed.myassistant
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.omarahmed.myassistant.onBoarding.OnBoarding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this,OnBoarding::class.java))
+            finish()
+        },2500)
+    }
+}
