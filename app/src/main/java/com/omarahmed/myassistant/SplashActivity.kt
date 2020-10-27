73966ed8141ba190b3f9221ca255239cacd8e12f
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-27 11:49:22 +0800

    add handleStartActivity function

--- app/src/main/java/com/omarahmed/myassistant/SplashActivity.kt
+++ app/src/main/java/com/omarahmed/myassistant/SplashActivity.kt
@@ -7,8 +7,10 @@ import android.os.Looper
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.omarahmed.myassistant.onBoarding.OnBoarding
+import com.omarahmed.myassistant.utils.SharedPreference
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -17,9 +19,21 @@ class SplashActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
-        Handler(Looper.myLooper()!!).postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            handleStartActivity()
+        },2500)
+    }
+
+    private fun handleStartActivity(){
+        val sharedPreference = SharedPreference(this)
+        val firstRun = sharedPreference.getBooleanValue("firstRun",true)
+        if (firstRun){
             startActivity(Intent(this,OnBoarding::class.java))
             finish()
-        },2500)
+        } else {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+        sharedPreference.putBooleanValue("firstRun",false)
     }
 }
