e29ab7c447ec29826f03c0d9789319aebcdbe699
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-10-27 21:24:33 +0800

    add SerializedName for countryCode

--- app/src/main/java/com/omarahmed/myassistant/data/remote/CountriesResponse.kt
+++ app/src/main/java/com/omarahmed/myassistant/data/remote/CountriesResponse.kt
@@ -12,6 +12,7 @@ data class CountriesResponse(
         data class CountriesInfo(
             @SerializedName("country_name")
             val countryName: String?,
+            @SerializedName("iso-3166")
             val isoName: String?,
             @SerializedName("total_holidays")
             val totalHolidays: Int,
