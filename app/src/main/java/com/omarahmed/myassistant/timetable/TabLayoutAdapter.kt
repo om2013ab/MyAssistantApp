b8e2c7782cdf2e34c744c4210d6d9eb5d24975a5
om2013ab <60521259+om2013ab@users.noreply.github.com>
2020-12-07 18:18:11 +0800

    move to viewPager2

--- app/src/main/java/com/omarahmed/myassistant/timetable/TabLayoutAdapter.kt
+++ app/src/main/java/com/omarahmed/myassistant/timetable/TabLayoutAdapter.kt
@@ -2,34 +2,16 @@ package com.omarahmed.myassistant.timetable
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.omarahmed.myassistant.timetable.daysFragments.*
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class TabLayoutAdapter(fm: FragmentManager): FragmentPagerAdapter(fm,
-    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-    override fun getItem(position: Int): Fragment {
-        return when(position){
-            0 -> MonFragment()
-            1 -> TuesFragment()
-            2 -> WedFragment()
-            3 -> ThurFragment()
-            4 -> FriFragment()
-            5-> SatFragment()
-            else -> SunFragment()
-        }
-    }
+class TabLayoutAdapter(
+    private val fragments: ArrayList<Fragment>,
+    fm: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fm, lifecycle) {
 
-    override fun getCount() = 7
+    override fun getItemCount() = fragments.size
+    override fun createFragment(position: Int) = fragments[position]
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when(position){
-            0-> "Mon"
-            1-> "Tues"
-            2-> "Wed"
-            3-> "Thur"
-            4-> "Fri"
-            5-> "Sat"
-            else -> "Sun"
-        }
-    }
 }
--- app/src/main/java/com/omarahmed/myassistant/timetable/TimetableFragment.kt
+++ app/src/main/java/com/omarahmed/myassistant/timetable/TimetableFragment.kt
@@ -10,21 +10,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.tabs.TabLayoutMediator
 import com.omarahmed.myassistant.R
+import com.omarahmed.myassistant.timetable.daysFragments.*
 import kotlinx.android.synthetic.main.fragment_timetable.*
 
-class TimetableFragment : Fragment(R.layout.fragment_timetable) {
+class TimetableFragment : Fragment(R.layout.fragment_timetable){
     private val timetableViewModel: TimetableViewModel by viewModels()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val tabLayoutAdapter = TabLayoutAdapter(childFragmentManager)
+        val fragments = arrayListOf(
+            MonFragment(),
+            TuesFragment(),
+            WedFragment(),
+            ThurFragment(),
+            FriFragment(),
+            SatFragment(),
+            SunFragment()
+        )
+        val tabLayoutAdapter = TabLayoutAdapter(fragments,requireActivity().supportFragmentManager,lifecycle)
         viewPager.adapter = tabLayoutAdapter
-        tabLayout.setupWithViewPager(viewPager)
-        timetableViewModel.getScheduleToCheckEmpty.observe(viewLifecycleOwner,{
-            timetableViewModel.checkEmptyTimetable(it)
-        })
+
+        TabLayoutMediator(tabLayout,viewPager){tab,position ->
+            when(position){
+                0 -> tab.text = "Mon"
+                1 -> tab.text = "Tues"
+                2 -> tab.text = "Wed"
+                3 -> tab.text = "Thur"
+                4 -> tab.text = "Fri"
+                5 -> tab.text = "Sat"
+                else -> tab.text = "Sun"
+            }
+        }.attach()
 
         setHasOptionsMenu(true)
         (activity as AppCompatActivity).setSupportActionBar(toolbarTimetable)
@@ -49,8 +67,6 @@ class TimetableFragment : Fragment(R.layout.fragment_timetable) {
         else{
             Toast.makeText(requireContext(),"You do not have any schedule to delete",Toast.LENGTH_SHORT).show()
         }
-
         return super.onOptionsItemSelected(item)
     }
-
 }
